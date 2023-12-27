702c0c0ca85c956e0184c536d2eed2d3816344cc
devaryakjha <dev.jha.arya@gmail.com>
2023-12-27 15:49:20 +0530

    Added `example` project

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.1+1
+
+- Added platforms to `pubspec.yaml`
+
 ## 1.0.0+1
 
 - Initial release
---
+++ example/android/app/src/main/kotlin/dev/aryak/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.aryak.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
